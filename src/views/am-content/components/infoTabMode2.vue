48d0f48977b5137992b3fc4b77d599f3a5477636
Jasonwang911 <396689144@qq.com>
2018-05-29 22:43:27 +0800

    解决bug’

--- src/views/am-content/components/infoTabMode2.vue
+++ src/views/am-content/components/infoTabMode2.vue
@@ -94,27 +94,11 @@
 </template>
 
 <script>
-import { fetchList, fetchPv, createArticle, updateArticle } from '@/api/article'
-import waves from '@/directive/waves' // 水波纹指令
-import { parseTime } from '@/utils'
-
-const calendarTypeOptions = [
-  { key: 'CN', display_name: 'China' },
-  { key: 'US', display_name: 'USA' },
-  { key: 'JP', display_name: 'Japan' },
-  { key: 'EU', display_name: 'Eurozone' }
-]
-
-// arr to obj ,such as { CN : "China", US : "USA" }
-const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
-  acc[cur.key] = cur.display_name
-  return acc
-}, {})
 
 export default {
   name: 'complexTable',
-  directives: {
-    waves
+  props: {
+    info: Object
   },
   data() {
     return {
